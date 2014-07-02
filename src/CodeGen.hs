82ef7cf1b858976d2eadb5100a6234df93eba4e4
lukasdittmann <l.dittmann@gmx.net>
2014-07-02 12:05:32 +0200

    CodeGen auf den neusten Stand gebracht (inkl. Bildverarbeitung)

--- src/CodeGen.hs
+++ src/CodeGen.hs
@@ -10,17 +10,38 @@ generateHTML ast = "<html>\n<head></head>\n<body>\n" ++ generateHTML' ast ++ "</
 
 -- dann Elemente für jeden AST-Knoten
 generateHTML' :: AST -> String
--- eine Sequenz
-generateHTML' (Sequence (a:as)) = generateHTML' a ++ "\n" ++ generateHTML' (Sequence as)
+
+-- eine Sequenz/Absatz
+generateHTML' (Sequence (a:as)) = generateHTML' a ++ "" ++ generateHTML' (Sequence as)
+generateHTML' (Absatz int ast)  = case int of
+    0 -> "" ++ (generateHTML' ast) ++ "\n"
+    _ -> "<p>\n" ++ (generateHTML' ast) ++ "\n</p>\n"
+
+-- ein Codeblock
+generateHTML' (Codeblock int ast)  = "<p style=\"border: "++ show (int+1) ++"px #000000 solid;\">\n" ++ (generateHTML' ast) ++ "\n</p>\n"
+
 -- eine Überschrift
-generateHTML' (H i str) = "<h" ++ show i ++ ">" ++ str ++ "</h" ++ show i ++ ">\n"
--- eine Überschrift
---generateHTML' (B str) = "<b>" ++ str ++ "</b>\n"
--- eine ungeordnete Liste
+generateHTML' (H i str) = "<h" ++ show i ++ ">" ++ str ++ "</h" ++ show i ++ ">"
+
+-- Bold und Kursiv
+generateHTML' (Bold ast) = "<b>" ++ concat (map generateHTML' ast) ++ "</b>"
+generateHTML' (Kursiv ast) = "<i>" ++ concat (map generateHTML' ast) ++ "</i>"
+
+-- Listen
 generateHTML' (UL lis) = "<ul>\n" ++ concat (map generateHTML' lis) ++ "</ul>\n"
+generateHTML' (OL lis) = "<ol>\n" ++ concat (map generateHTML' lis) ++ "</ol>\n"
+
 -- Listenelemente
---generateHTML' (LI str) = "<li>" ++ str ++ "</li>\n"
--- ein Absatz
-generateHTML' (P str)  = "<p>" ++ str ++ "</p>\n"
--- alles andere (?) wird für den Moment ignoriert
+generateHTML' (LiE ast) = "<li>" ++ concat (map generateHTML' ast) ++ "</li>\n"
+
+-- Bilder
+generateHTML' (Image str) = "<img src=\""++ str ++"\"/>\n"
+
+-- Zeilenumbruch
+generateHTML' (Break) = "<br>\n"
+
+-- ein Textblock
+generateHTML' (Text str)  = "" ++ str ++ ""
+
+-- alles andere wird für den ignoriert
 generateHTML' _ = ""
