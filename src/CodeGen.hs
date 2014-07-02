93438b0125f3c3541578a8fcdd19c98fbea733e2
lukasdittmann <l.dittmann@gmx.net>
2014-07-03 01:24:00 +0200

    direct links fixed and ready for ref links

--- src/CodeGen.hs
+++ src/CodeGen.hs
@@ -3,45 +3,59 @@ module CodeGen where
 
 import IR
 
+import Data.Map as M
+
 -- HTML generieren
 -- zuerst das äußere Gerüst
-generateHTML :: AST -> String
-generateHTML ast = "<html>\n<head></head>\n<body>\n" ++ generateHTML' ast ++ "</body>\n</html>"
+generateHTML :: AST-> References -> String
+generateHTML ast linkList= "<html>\n<head></head>\n<body>\n" ++ generateHTML' (ast,linkList)++ "</body>\n</html>"
 
 -- dann Elemente für jeden AST-Knoten
-generateHTML' :: AST -> String
+generateHTML' :: (AST,References) -> String
 
 -- eine Sequenz/Absatz
-generateHTML' (Sequence (a:as)) = generateHTML' a ++ "" ++ generateHTML' (Sequence as)
-generateHTML' (Absatz int ast)  = case int of
-    0 -> "" ++ (generateHTML' ast) ++ "\n"
-    _ -> "<p>\n" ++ (generateHTML' ast) ++ "\n</p>\n"
+generateHTML' ((Sequence (a:as)),linkList)= generateHTML' (a, linkList)++ "" ++ generateHTML' ((Sequence as), linkList)
+generateHTML' ((Absatz int ast),  linkList)= case int of
+    0 -> "" ++ (generateHTML' (ast, linkList)) ++ "\n"
+    _ -> "<p>\n" ++ (generateHTML' (ast, linkList)) ++ "\n</p>\n"
 
 -- ein Codeblock
-generateHTML' (Codeblock int ast)  = "<p style=\"border: "++ show (int+1) ++"px #000000 solid;\">\n" ++ (generateHTML' ast) ++ "\n</p>\n"
+generateHTML' ((Codeblock int ast), linkList)= "<p style=\"border: "++ show (int+1) ++"px #000000 solid;\">\n" ++ (generateHTML' (ast, linkList)) ++ "\n</p>\n"
 
 -- eine Überschrift
-generateHTML' (H i str) = "<h" ++ show i ++ ">" ++ str ++ "</h" ++ show i ++ ">"
+generateHTML' ((H i str),linkList)= "<h" ++ show i ++ ">" ++ str ++ "</h" ++ show i ++ ">"
 
 -- Bold und Kursiv
-generateHTML' (Bold ast) = "<b>" ++ concat (map generateHTML' ast) ++ "</b>"
-generateHTML' (Kursiv ast) = "<i>" ++ concat (map generateHTML' ast) ++ "</i>"
+generateHTML' ((Bold ast),linkList) = "<b>" ++ generateHTML' (Sequence ast,linkList) ++ "</b>"
+generateHTML' ((Kursiv ast),linkList)= "<i>" ++ generateHTML' (Sequence ast,linkList) ++ "</i>"
 
 -- Listen
-generateHTML' (UL lis) = "<ul>\n" ++ concat (map generateHTML' lis) ++ "</ul>\n"
-generateHTML' (OL lis) = "<ol>\n" ++ concat (map generateHTML' lis) ++ "</ol>\n"
+generateHTML' ((UL lis),linkList)= "<ul>\n" ++ generateHTML' (Sequence lis,linkList) ++ "</ul>\n"
+generateHTML' ((OL lis),linkList)= "<ol>\n" ++ generateHTML' (Sequence lis,linkList) ++ "</ol>\n"
 
 -- Listenelemente
-generateHTML' (LiE ast) = "<li>" ++ concat (map generateHTML' ast) ++ "</li>\n"
+generateHTML' ((LiE ast),linkList)= "<li>" ++ generateHTML' (Sequence ast,linkList) ++ "</li>\n"
 
 -- Bilder
-generateHTML' (Image str) = "<img src=\""++ str ++"\"/>\n"
+generateHTML' ((Image str),linkList)= "<img src=\""++ str ++"\"/>\n"
 
 -- Zeilenumbruch
-generateHTML' (Break) = "<br>\n"
+generateHTML' ((Break),linkList)= "<br>\n"
 
 -- ein Textblock
-generateHTML' (Text str)  = "" ++ str ++ ""
+generateHTML' ((Text str),linkList) = "" ++ str ++ ""
 
+-- Referenz - Links umsetzen
+generateHTML' ((Link id text),linkList) = 
+    let ref = M.lookup id linkList
+    in (case ref of
+        Nothing  -> "<a href=\"#\">"++ text ++"</a>"
+        Just url -> "<a href=\"" ++ url
+                    ++ "\">"++ text ++"</a>"
+     )
+
+-- Direkte Links übersetzen
+generateHTML' ((DLink url text),linkList) = "<a href=\""++ url ++"\">"++ text ++"</a>"
+     
 -- alles andere wird für den ignoriert
 generateHTML' _ = ""
